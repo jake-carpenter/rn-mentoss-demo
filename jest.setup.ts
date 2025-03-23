e05355a92559e26c3430bd2599475f15e3b133f0
Jake Carpenter <jake@jakecarpenter.dev>
2025-03-22 21:46:46 -0600

    Install Mentoss

---
+++ jest.setup.ts
@@ -0,0 +1,20 @@
+import { setUpTests } from "react-native-reanimated";
+// import { FetchMocker, MockServer } from "mentoss";
+
+setUpTests();
+
+// Uncommenting this will cause a failure
+// const server = new MockServer("http://localhost");
+// const mocker = new FetchMocker({ servers: [server] });
+
+// beforeAll(() => {
+//   mocker.mockGlobal();
+// });
+
+// afterEach(() => {
+//   mocker.clearAll();
+// });
+
+// afterAll(() => {
+//   mocker.unmockGlobal();
+// });
